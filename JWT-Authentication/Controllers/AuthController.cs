4a66011dfb70ab34975606f9df80893d01755685
ngothanhdat-AK <ngothanhdat4002@gmail.com>
2025-06-08 07:16:27 +0700

    Implement JWT authentication and user management
    - Created `UserDto` class for user data handling.
    - Enhanced `AuthController` for user registration and login, including JWT token generation.
    - Removed `WeatherForecastController` to streamline functionality.
    - Updated project file to include `System.IdentityModel.Tokens.Jwt` package.
    - Modified `appsettings.Development.json` to add JWT token configuration.

---
+++ JWT-Authentication/Controllers/AuthController.cs
@@ -0,0 +1,69 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using JWT_Authentication.Abstraction.Entities;
+using JWT_Authentication.Abstraction.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace JWT_Authentication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController(IConfiguration configuration) : ControllerBase
+    {
+        public static User user = new User();
+
+        [HttpPost("register")]
+        public ActionResult<User> Register(UserDto request)
+        {
+            var hashedPassword = new PasswordHasher<User>()
+                .HashPassword(user, request.Password);
+           
+            user.Username = request.Username;
+            user.PasswordHash = hashedPassword;
+
+            return Ok(user);
+        }
+
+        [HttpPost("login")]
+        public ActionResult<string> Login(UserDto request)
+        {
+            if(user.Username != request.Username)
+            {
+                return BadRequest("User not found");
+            }
+            if(new PasswordHasher<User>().VerifyHashedPassword(user, user.PasswordHash, request.Password) == PasswordVerificationResult.Failed)
+            {
+                return BadRequest("Wrong password");
+            }
+
+            string token = GenerateToken(user);
+            return Ok(token);
+        }
+        
+        private string GenerateToken(User user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.Username),
+            };
+
+            var key = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(configuration.GetValue<string>("Token:Key")!)); ;
+
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var tokenDescription = new JwtSecurityToken(
+                issuer: configuration.GetValue<string>("Token:Issuer"),
+                audience: configuration.GetValue<string>("Token:Audience"),
+                claims: claims,
+                expires: DateTime.Now.AddDays(1),
+                signingCredentials: credentials
+                );
+
+            return new JwtSecurityTokenHandler().WriteToken(tokenDescription);
+        }
+    }
+}
